50efabd536d7a46e3817ad105c9406be4c688781
Jadan Kerr <badi1863@gmail.com>
2023-09-27 17:17:40 -0600

    Wrote menu printer!

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your starting string?");
+        String response = scanner.nextLine();
+        menuPrinter(response);
+    }
+    public static String menuPrinter(String input){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("The string you inputted is: \n");
+        System.out.println(input + "\n");
+        System.out.println("What would you like to do with your string?");
+        System.out.println("A) Add more content to the string");
+        System.out.println("B) Check if string contains a substring");
+        System.out.println("C) Output the length of a string");
+        System.out.println("D) Output the substring after a given prefix");
+        System.out.println("E) Output the substring before a given prefix");
+        System.out.println("F) Output the substring before and after a given prefix and suffix");
+        System.out.println("G) Exit the program");
+        return scanner.nextLine();
+
+
+
+
+
+
+    }
+}
